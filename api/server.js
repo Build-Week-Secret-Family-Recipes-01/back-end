322c67b9b2b2ef04d620bf24ec6dd0af1e808ab6
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2022-01-30 15:35:57 -0600

    Updating dependencies - cannot get Authorization tokan to read permissions on my tests for some reason

--- api/server.js
+++ api/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-const path = require("path");
 
 const authRouter = require('./auth/auth-router');
 const usersRouter = require('./users/users-router');
@@ -10,14 +9,9 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
-server.use(express.static(path.join(__dirname, "../client")));
 
 server.use("/api/auth", authRouter);
 server.use("/api/users", usersRouter);
-
-server.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client", "index.html"));
-});
 
 server.use((err, req, res, next) => {
   res.status(err.status || 500).json({
