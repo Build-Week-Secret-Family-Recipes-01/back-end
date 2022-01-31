c6cdcb320bd15b4fdd0ebb92f1bc243ecdbbe893
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2022-01-31 14:27:01 -0600

    Got the getRecipe function working completely with some messy JS magic

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -1,3 +1,4 @@
+const { raw } = require("express");
 const db = require("../data/db-config");
 
 async function getAllRecipes() {
@@ -21,15 +22,83 @@ async function getSteps(recipe_id) {
 }
 
 async function getRecipe(filter) {
-  const recipe = await db("recipes as r")
-  .join("steps as s")
-  // filter by recipeid, get an array with redundant data - use JS to hammer that into place - join all 4 tables, then filter
-  .where(filter).first();
-  recipe.steps = await getSteps(recipe.recipe_id);
-  // recipe.steps.forEach(async step => {
-  //   step.ingredients = getIngredients(step.step_id);
-  // });
+  const rawData = await db("recipes as r")
+    .join("steps as s", "s.recipe_id", "r.recipe_id")
+    .join("recipe_categories as rcat", "rcat.recipe_id", "r.recipe_id")
+    .join("categories as cat", "cat.category_id", "rcat.category_id")
+    .leftJoin("steps_ingredients as si", "si.step_id", "s.step_id")
+    .leftJoin("ingredients as i", "si.ingredient_id", "i.ingredient_id")
+    .select(
+      "r.recipe_id",
+      "r.title",
+      "r.source",
+      "r.image",
+      "r.user_id",
+      "cat.category_name",
+      "s.step_number",
+      "s.step_text",
+      "si.quantity",
+      "i.ingredient_name"
+    )
+    .orderBy("s.step_number")
+    .where(filter);
+
+  let recipe = {
+    recipe_id: rawData[0].recipe_id,
+    user_id: rawData[0].user_id,
+    title: rawData[0].title,
+    source: rawData[0].source,
+    image: rawData[0].image,
+    category_name: rawData[0].category_name,
+    steps: [],
+  };
+
+  for (let i = 0; i < rawData.length; i++) {
+    let pushStep = () => {
+      let step = {
+        step_number: rawData[i].step_number,
+        step_text: rawData[i].step_text,
+        ingredients: [
+          {
+            Quantity: rawData[i].quantity,
+            Ingredient: rawData[i].ingredient_name,
+          },
+        ],
+      };
+      recipe.steps.push(step);
+    };
+
+    if (i > 0) {
+      if (rawData[i].step_number === rawData[i - 1].step_number) {
+        recipe.steps[i - 1].ingredients.push({
+          Quantity: rawData[i].quantity,
+          Ingredient: rawData[i].ingredient_name,
+        });
+      } else {
+        pushStep();
+      }
+    } else {
+      pushStep();
+    }
+  }
+
   return recipe;
 }
 
 module.exports = { getAllRecipes, getRecipe };
+
+// SQL for the GetRecipe function:
+// select
+// 	r.recipe_id, r.title, r.source, r.image, r.user_id, cat.category_name, s.step_number, s.step_text, si.quantity, i.ingredient_name
+// from recipes as r
+// join steps as s
+// 	on s.recipe_id = r.recipe_id
+// join recipe_categories as rcat
+// 	on rcat.recipe_id = r.recipe_id
+// join categories as cat
+// 	on cat.category_id = rcat.category_id
+// left join steps_ingredients as si
+// 	on si.step_id = s.step_id
+// join ingredients as i
+// 	on si.ingredient_id = i.ingredient_id
+// where r.recipe_id = 1;
